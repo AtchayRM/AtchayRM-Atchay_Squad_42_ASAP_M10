527fcf8d90c7b71a8f1ad93805f986bb2828f6b6
AtchayRM <Atchay.kumaran.s75@kalvium.community>
2025-03-05 10:18:53 +0530

    Frontend Deployment

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      Name : R S Sujaykiran
+      <br />
+      Designation : Kalvium student
+      <br />
+      Address : Vels University
+      <br />
+      Email : r.ssujaykiran.s75@kalvium.community
+    </div>
+  )
+}
+
+export default App
